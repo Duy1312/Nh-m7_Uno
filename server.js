8cb23e39d20d88e80ee03632544996f7ad9cf5be
Duy Nguyễn <duynguyen@Duys-MacBook-Pro-3.local>
2022-11-22 14:22:17 +0700

    Create Start Project Structure

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+​
+app.use(express.static(__dirname + '/public'));
+io.on('connection', onConnection);
+http.listen(port, () => console.log('listening on port ' + port));
+​
+function onConnection(socket) {
+   console.log('a user connected');
+}
